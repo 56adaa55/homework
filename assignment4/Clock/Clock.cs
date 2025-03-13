6498d5e52fd4fa26af8fa3bc2d4aa5cd2146f3cf
lzy <2816193327@qq.com>
2025-03-13 11:01:13 +0800

    Add assignment4 code

---
+++ assignment4/Clock/Clock.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public delegate void EventHandler(object sender,DateTime datetime);
+
+namespace Clock
+{
+    class Event
+    {
+        public event EventHandler OnClock;
+        public void OnEvent(DateTime dateTime)
+        {
+            // 确保事件被触发时，已订阅处理函数
+            OnClock?.Invoke(this, dateTime);
+        }
+    }
+    class Clock
+    {
+        public Event clock = new Event();
+        private void Tick(object sender,DateTime dateTime)
+        {
+            DateTime cur_time = DateTime.Now;
+            Console.WriteLine($"Clock runs!The time is {cur_time}");
+        }
+        private void alarm(object sender,DateTime dateTime)
+        {
+            DateTime cur_time = DateTime.Now;
+            if (cur_time.ToString("yyyy-MM-dd HH:mm:ss") == dateTime.ToString("yyyy-MM-dd HH:mm:ss"))
+                Console.WriteLine($"Clock alarms!");
+        }
+        public  Clock()
+        {
+            clock.OnClock += Tick;
+            clock.OnClock += alarm;
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            Clock clock_object = new Clock();
+            // 提示用户输入日期和时间
+            Console.WriteLine("请输入日期和时间 (格式: yyyy-MM-dd HH:mm:ss):");
+
+            // 读取用户输入
+            string userInput = Console.ReadLine();
+            DateTime parsedDateTime;
+            if (DateTime.TryParse(userInput, out parsedDateTime))
+            {
+                Console.WriteLine("输入的时间是: " + parsedDateTime);
+            }
+            else
+            {
+                Console.WriteLine("输入的时间格式无效，请确保格式为 yyyy-MM-dd HH:mm:ss.");
+                Environment.Exit(0);
+            }
+            int i = 0;
+            while(i<180)  //最多循环120秒
+            { 
+                clock_object.clock.OnEvent(parsedDateTime);
+                Thread.Sleep(1000);
+                i++;
+            }
+        }
+    }
+}
---
+++ assignment4/List/List.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    public class List<T>
+    {
+        public class Node
+        {
+            public T data;
+            public Node next;
+            public Node(T d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+        private Node head;
+        public List()
+        {
+            head = null;
+        }
+        public void add(T data)
+        {
+            Node new_node = new Node(data);
+            if (head == null)
+                head = new_node;
+            else
+            {
+                Node cur = head;
+                while (cur.next != null)
+                    cur = cur.next;
+                cur.next = new_node;
+            }
+        }
+        public void Foreach(Action<T> action)
+        {
+            Node cur = head;
+            while(cur!=null)
+            {
+                action(cur.data);
+                cur = cur.next;
+            }
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            List<int> mylist = new List<int>();
+            Console.WriteLine("请输入数据：");
+            string input = Console.ReadLine();
+            string[] parts = input.Split(' '); // 使用空格作为分隔符
+            // 将每个字符串转换为整数
+            int[] numbers = parts.Select(x => Convert.ToInt32(x)).ToArray();
+            for (int i = 0; i < numbers.Length; i++)
+                mylist.add(numbers[i]);
+            Console.Write("数组里的元素依次为：");
+            mylist.Foreach(m => Console.Write(m + " "));
+            Console.WriteLine();
+            int max = int.MinValue;
+            mylist.Foreach(m => { if (m > max) max = m; });
+            Console.Write($"数组里的最大值为：{max}");
+            Console.WriteLine();
+            int min = int.MaxValue;
+            mylist.Foreach(m => { if (m < min) min = m; });
+            Console.Write($"数组里的最大值为：{min}");
+            Console.WriteLine();
+            int sum = 0;
+            mylist.Foreach(m => sum = sum + m);
+            Console.Write($"数组里元素的和为：{sum}");
+        }
+    }
+}
